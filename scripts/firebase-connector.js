447276abe377004ec8afccb0f26fb4a62f3d3229
Nora-Zhang <60050372+Nora-Zhang@users.noreply.github.com>
2021-07-19 02:28:13 +0800

    Update firebase-connector.js

--- scripts/firebase-connector.js
+++ scripts/firebase-connector.js
@@ -1,6 +1,4 @@
-  import firebase from "firebase/app";
-  import "firebase/analytics";
-
+  
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
@@ -8,6 +6,7 @@
     authDomain: "first-learning-tool.firebaseapp.com",
     projectId: "first-learning-tool",
     storageBucket: "first-learning-tool.appspot.com",
+    databaseURL: "https://first-learning-tool-default-rtdb.firebaseio.com",
     messagingSenderId: "599129377890",
     appId: "1:599129377890:web:d324084086695eaed0d53b",
     measurementId: "G-QK2SVJZ7MN"
