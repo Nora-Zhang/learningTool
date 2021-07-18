4052403e0d1023cfa3795b360111753ddfb839eb
Nora-Zhang <60050372+Nora-Zhang@users.noreply.github.com>
2021-07-19 01:09:20 +0800

    Create firebase-connector.js

---
+++ scripts/firebase-connector.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCwrUsWj1TIovFni6dt1vzLylw6UjoIfk4",
+    authDomain: "first-learning-tool.firebaseapp.com",
+    projectId: "first-learning-tool",
+    storageBucket: "first-learning-tool.appspot.com",
+    messagingSenderId: "599129377890",
+    appId: "1:599129377890:web:d324084086695eaed0d53b",
+    measurementId: "G-QK2SVJZ7MN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
